6be37bbccbf101ca00cfbb5780fcd10ab5ab596f
Buh Khuu <buhkhuu@BuhMac.local>
2022-09-03 16:49:04 +0200

    20220903

---
+++ Functions.py
@@ -0,0 +1,129 @@
+from xml.sax import default_parser_list
+from flask import Flask, flash, redirect, render_template, request, session, url_for, jsonify, abort
+import sqlite3
+
+from mollie.api.client import Client
+from mollie.api.error import Error
+import mollie
+
+def getData(mode="all"):
+    conn = sqlite3.connect('Volleyball.db')
+    c = conn.cursor()
+
+    match_list = {i[0]: list(i[1:]) for i in c.execute("SELECT * FROM MATCHDAYS").fetchall()}
+    player_list = {i[0]:' '.join(i[1:]) for i in c.execute("SELECT * FROM PLAYER").fetchall()}
+    data_list = {i[0]: list(i[1:]) for i in c.execute("SELECT * FROM DATA").fetchall()}
+    location_list = {i[0]: list(i[1:]) for i in c.execute("SELECT * FROM LOCATIONS").fetchall()}
+
+    # Data_list manupalation:
+    #   {i: [name, match, new, tent, p_id]}
+    for (n, data) in data_list.items():
+        data_list[n][0] = player_list[data[0]]
+        data_list[n][1] = match_list[data[1]][0]
+
+    match_n = {data[0]:0 for i, data in match_list.items()}
+    for i in [data[1] for j,data in data_list.items()]:
+        match_n[i] = match_n[i] + 1
+
+    match_list = {i: data + [match_n[data[0]]] for i, data in match_list.items()}
+
+    if mode == "match":
+        return match_list
+    elif mode == "player":
+        return player_list
+    elif mode == "location":
+        return location_list
+    elif mode == "all":
+        return data_list
+    else:
+        print("Empty")
+        return 0
+
+def create_payment(matchday, names, price):
+    api_key = "test_k9tafBmqUy3ATSVUVywAjGqtAqhVR7"
+    mollie_client = Client()
+    mollie_client.set_api_key(api_key)
+
+    my_webshop_id = "test"
+    payment = mollie_client.payments.create(
+        {
+            "amount": {"currency": "EUR", "value": (price)},
+            "description": f"Volleybal betaling {matchday}",
+            "webhookUrl": f"http://143.177.144.137/webhook",
+            "redirectUrl": f"http://143.177.144.137/payment?matchday={matchday}&message=",
+            "metadata": {"my_webshop_id": str(my_webshop_id)},
+            "method": "ideal"
+        }
+    )
+
+    data = {"status": payment.status}
+    print(payment)
+
+    conn = sqlite3.connect('Volleyball.db')
+    c = conn.cursor()
+
+    for name in names:
+        firstname, lastname = name.split(' ')[0], ' '.join(name.split(' ')[1:])
+        player_id = c.execute(f"SELECT id FROM PLAYER WHERE firstname='{firstname}' AND lastname='{lastname}'").fetchall()[0][0]
+        match_id = c.execute(f"SELECT id FROM MATCHDAYS WHERE date='{matchday}'").fetchall()[0][0]
+
+        print(player_id, match_id, payment.id)
+
+        #  ONLY DO THIS WHEN IT GENUINLY SAYS 0 AT PAID
+        c.execute(f"UPDATE DATA SET payment_id='{payment.id}' WHERE player_id='{player_id}' AND match_id='{match_id}'")
+    conn.commit()
+
+    return payment.checkout_url
+
+def signupcheck(matchday, signupname):
+    # Existence Check
+    search_list = [(key, value) for key, value in getData("player").items() if value == signupname]
+    if len(search_list) == 0:
+        return "unknown_p"
+
+    # Capacity Check
+    max_limit = [i for i in list(getData("match").values()) if i[0]==matchday][0][4]
+    n_participants = len([i for i in list(getData().values()) if i[1] == matchday])
+    if n_participants >= max_limit:
+        return "full"
+
+    # Duplicate Check
+    participants = [i for i in list(getData().values()) if i[1] == matchday]
+    for i in participants:
+        if i[0] == signupname:
+            return "duplicate"
+    
+    # Payment Check
+    player_history = [i for i in list(getData().values()) if i[0] == signupname]
+    payment_history = [0 if i[4] == 'NULL' else 1 for i in player_history]
+
+    if sum(payment_history) != len(payment_history):
+        return "not_paid"
+
+    return "allowed"
+
+def get_id(mode, search):
+    if mode == "match":
+        ids = {key: value for key, value in getData(mode).items() if value[0] == search or value[0] in search}
+    elif mode == "player":
+        ids = {key: value for key, value in getData(mode).items() if value == search or value in search}
+    else:
+        ids = dict()
+    return ids
+
+def match_webdeatils(matchday):
+    conn = sqlite3.connect('Volleyball.db')
+    c = conn.cursor()
+
+    match_list = [[key] + value[:-1] for key, value in getData("match").items() if value[0] == matchday][0]
+    match_list.append(c.execute(f"SELECT details FROM LOCATIONS WHERE name='{match_list[4]}'").fetchall()[0][0])
+    participants_data = [i for i in list(getData().values()) if i[1]==matchday]
+    participants_data.sort(key=lambda row: (row[0]))
+
+    participant_names = [i[0] for i in participants_data]
+    all_names = set(getData("player").values())
+
+    names = list(all_names.difference(set(participant_names)))
+    names.sort()
+
+    return match_list, participants_data, names
--- Program.py
+++ Program.py
@@ -3,6 +3,7 @@ import sqlite3
 import os.path as path
 import datetime
 import requests
+import Functions
 
 import os
 import time
@@ -12,76 +13,6 @@ from mollie.api.error import Error
 import mollie
 
 app = Flask(__name__)
-
-def getData(mode="all"):
-    conn = sqlite3.connect('Volleyball.db')
-    c = conn.cursor()
-
-    match_list = {i[0]: list(i[1:]) for i in c.execute("SELECT * FROM MATCHDAYS").fetchall()}
-    player_list = {i[0]:' '.join(i[1:]) for i in c.execute("SELECT * FROM PLAYER").fetchall()}
-    data_list = {i[0]: list(i[1:]) for i in c.execute("SELECT * FROM DATA").fetchall()}
-    location_list = {i[0]: list(i[1:]) for i in c.execute("SELECT * FROM LOCATIONS").fetchall()}
-
-    # Data_list manupalation:
-    #   {i: [name, match, new, tent, p_id]}
-    for (n, data) in data_list.items():
-        data_list[n][0] = player_list[data[0]]
-        data_list[n][1] = match_list[data[1]][0]
-
-    match_n = {data[0]:0 for i, data in match_list.items()}
-    for i in [data[1] for j,data in data_list.items()]:
-        match_n[i] = match_n[i] + 1
-
-    match_list = {i: data + [match_n[data[0]]] for i, data in match_list.items()}
-
-    if mode == "match":
-        return match_list
-    elif mode == "player":
-        return player_list
-    elif mode == "locations":
-        return location_list
-    elif mode == "all":
-        return data_list
-    else:
-        print("Empty")
-        return 0
-    
-def create_payment(matchday, names, price):
-    api_key = "test_k9tafBmqUy3ATSVUVywAjGqtAqhVR7"
-    mollie_client = Client()
-    mollie_client.set_api_key(api_key)
-
-    my_webshop_id = int(time.time())
-    payment = mollie_client.payments.create(
-        {
-            "amount": {"currency": "EUR", "value": (price)},
-            "description": f"Volleybal betaling {matchday}",
-            "webhookUrl": f"http://143.177.144.137/webhook",
-            "redirectUrl": f"http://143.177.144.137/payment?matchday={matchday}&message=",
-            "metadata": {"my_webshop_id": str(my_webshop_id)},
-            "method": "ideal"
-        }
-    )
-
-    data = {"status": payment.status}
-    print(payment)
-
-    conn = sqlite3.connect('Volleyball.db')
-    c = conn.cursor()
-
-    for name in names:
-        firstname, lastname = name.split(' ')[0], ' '.join(name.split(' ')[1:])
-        player_id = c.execute(f"SELECT id FROM PLAYER WHERE firstname='{firstname}' AND lastname='{lastname}'").fetchall()[0][0]
-        match_id = c.execute(f"SELECT id FROM MATCHDAYS WHERE date='{matchday}'").fetchall()[0][0]
-
-        print(player_id, match_id, payment.id)
-
-        #  ONLY DO THIS WHEN IT GENUINLY SAYS 0 AT PAID
-        c.execute(f"UPDATE DATA SET payment_id='{payment.id}' WHERE player_id='{player_id}' AND match_id='{match_id}'")
-    conn.commit()
-
-    return payment.checkout_url
-
 
 # REMOVE CACHING POSSIBILITIES [PDF WILL UPDATE AFTER REFRESH]
 
@@ -126,7 +57,8 @@ def webhook():
 
 @app.route('/')
 def index():
-    print(request.environ.get("REMOTE_ADDR"))
+    for key, value in request.environ.items():
+        print(f"{key}\t\t {value}")
     return render_template('home.html')
 
 # ADMIN ADD LOCATION
@@ -153,7 +85,7 @@ def admin():
     if request.method == 'POST':
         matchday = int(request.form.get('dateBtn'))
         return redirect(url_for('adminmatch', matchday = matchday, message = ''))
-    match_list = getData("match")
+    match_list = Functions.getData("match")
     return render_template('admin.html', list=list(match_list.values())[::-1], n_list=len(match_list))
     
 # ADMIN: MATCH MANAGEMENT
@@ -161,7 +93,7 @@ def admin():
 @app.route('/adminmatch', methods=['GET', 'POST'])
 def adminmatch():
     matchday = request.args['matchday']
-    matchday_id = [key for key, value in getData("match").items() if value[0] == matchday][0]
+    matchday_id = [key for key, value in Functions.getData("match").items() if value[0] == matchday][0]
     message = request.args['message']
     
     conn = sqlite3.connect('Volleyball.db')
@@ -186,9 +118,9 @@ def adminmatch():
 
         if status == "open":
             if len(request.form.get("uname")) == 0:
-                participants = [j[0] for j in [i for i in list(getData().values()) if i[1] == matchday]]
+                participants = [j[0] for j in [i for i in list(Functions.getData().values()) if i[1] == matchday]]
                 unsubscribers = list(set([name for name in participants if request.form.get(name) == 'on']))
-                unsubscribers_ids = [key for key, value in getData("player").items() if value in unsubscribers]
+                unsubscribers_ids = [key for key, value in Functions.getData("player").items() if value in unsubscribers]
                 matchday_id = c.execute(f"SELECT id FROM MATCHDAYS WHERE date='{matchday}'").fetchall()[0][0]
 
                 for n in unsubscribers_ids:
@@ -204,13 +136,13 @@ def adminmatch():
                 signup_firstname, signup_lastname = request.form["uname"].split()[0], ' '.join(request.form["uname"].split()[1:])
                 
                 # Capacity Check
-                max_limit = [i for i in list(getData("match").values()) if i[0]==matchday][0][4]
-                n_participants = len([i for i in list(getData().values()) if i[1] == matchday])
+                max_limit = [i for i in list(Functions.getData("match").values()) if i[0]==matchday][0][4]
+                n_participants = len([i for i in list(Functions.getData().values()) if i[1] == matchday])
                 if n_participants >= max_limit:
                     return redirect(url_for('adminmatch', matchday = matchday, message = "Limiet bereikt."))
 
                 # Duplicate Check
-                participants = [i for i in list(getData().values()) if i[1] == matchday]
+                participants = [i for i in list(Functions.getData().values()) if i[1] == matchday]
                 for i in participants:
                     if i[0] == signupname:
                         return redirect(url_for('adminmatch', matchday = matchday, message = signupname + " staat al ingeschreven."))
@@ -218,7 +150,7 @@ def adminmatch():
                 # Payment Check
                 player_id = c.execute(f"SELECT id FROM PLAYER WHERE firstname='{signup_firstname}' AND lastname='{signup_lastname}'").fetchall()[0][0]
                 print("---------------\n",player_id,"\n---------------\n")
-                player_history = [i for i in list(getData().values()) if i[0] == signupname]
+                player_history = [i for i in list(Functions.getData().values()) if i[0] == signupname]
                 print("---------------\n",player_history,"\n---------------\n")
                 payment_history = [0 if i[4] == 'NULL' else 1 for i in player_history]
                 print("---------------\n",payment_history,"\n---------------\n")
@@ -233,9 +165,9 @@ def adminmatch():
                 return redirect(url_for('adminmatch', matchday = matchday, message = 'Je bent toegevoegd!'))  
         elif status == "ended": 
             print(request.form)
-            participants = [j[0] for j in [i for i in list(getData().values()) if i[1] == matchday]]
+            participants = [j[0] for j in [i for i in list(Functions.getData().values()) if i[1] == matchday]]
             payers = list(set([name for name in participants if request.form.get(name) == 'on']))
-            payers_ids = [key for key, value in getData("player").items() if value in payers]
+            payers_ids = [key for key, value in Functions.getData("player").items() if value in payers]
             matchday_id = c.execute(f"SELECT id FROM MATCHDAYS WHERE date='{matchday}'").fetchall()[0][0]
 
             print(participants, payers, payers_ids, matchday_id)
@@ -256,17 +188,7 @@ def adminmatch():
             new_message = f"Volgende spelers zijn gemakeerd als betaald: \n {' - '.join(payers)}"
             return redirect(url_for('adminmatch', matchday = matchday, message = new_message))
 
-    match_list = list(c.execute("SELECT * FROM MATCHDAYS WHERE date="+str(matchday)).fetchall()[0])
-    match_list.append(c.execute(f"SELECT details FROM LOCATIONS WHERE name='{match_list[4]}'").fetchall()[0][0])
-    participants_data = [i for i in list(getData().values()) if i[1]==matchday]
-    participants_data.sort(key=lambda row: (row[0]))
-    # player_data = c.execute("SELECT * FROM DATA WHERE match_id="+str(match_list[0])).fetchall()
-    print(participants_data)
-    participant_names = [i[0] for i in participants_data]
-    all_names = set(getData("player").values())
-
-    names = list(all_names.difference(set(participant_names)))
-    names.sort()
+    match_list, participants_data, names = Functions.match_webdeatils(matchday)
 
     return render_template('adminmatch.html', match_list = match_list, participants_data = participants_data, n_players = len(participants_data), message = message, names = names, n_names = len(names))
 
@@ -274,7 +196,7 @@ def adminmatch():
 
 @app.route('/admincreatematch', methods=['GET', 'POST'])
 def admincreatematch():
-    location_list = (getData("locations"))
+    location_list = (Functions.getData("location"))
     if request.method == 'POST':
 
         date = ''.join(request.form.get('date').split('-'))
@@ -300,14 +222,14 @@ def admincreatematch():
 def matches():
     if request.method == 'POST':
         matchday = int(request.form.get('dateBtn'))
-        open = [i for i in list(getData("match").values()) if i[0] == str(matchday)][0]
+        open = [i for i in list(Functions.getData("match").values()) if i[0] == str(matchday)][0]
         print(open)
         if open[5] == "ended":
             return redirect(url_for('payment', matchday = matchday, message = ''))
         else:
             return redirect(url_for('signup', matchday = matchday, message = ''))
 
-    match_list = getData("match")
+    match_list = Functions.getData("match")
 
     return render_template('matches.html', list=list(match_list.values())[::-1], n_list=len(match_list))
 
@@ -322,7 +244,7 @@ def payment():
     c = conn.cursor()
 
     if request.method == 'POST':
-        not_paid = [j[0] for j in [i for i in list(getData().values()) if i[5] == 0 and i[1] == matchday]]
+        not_paid = [j[0] for j in [i for i in list(Functions.getData().values()) if i[5] == 0 and i[1] == matchday]]
         print(not_paid)
 
         going2pay = list(set([name for name in not_paid if request.form.get(name) == 'on']))
@@ -333,26 +255,15 @@ def payment():
         if n_payments == 0:
             return redirect(url_for('payment', matchday = matchday, message = 'Geen personen geselecteerd')) 
 
-        price = float([i for i in list(getData("match").values()) if i[0] == matchday][0][6])
+        price = float([i for i in list(Functions.getData("match").values()) if i[0] == matchday][0][6])
         print(price)
 
         total_price = n_payments * price
 
-        checkout_url = create_payment(matchday, going2pay, "{:.2f}".format(total_price))
+        checkout_url = Functions.create_payment(matchday, going2pay, "{:.2f}".format(total_price))
         return redirect(checkout_url)
                 
-
-    match_list = list(c.execute("SELECT * FROM MATCHDAYS WHERE date="+str(matchday)).fetchall()[0])
-    match_list.append(c.execute(f"SELECT details FROM LOCATIONS WHERE name='{match_list[4]}'").fetchall()[0][0])
-    participants_data = [i for i in list(getData().values()) if i[1]==matchday]
-    participants_data.sort(key=lambda row: (row[0]))
-    # player_data = c.execute("SELECT * FROM DATA WHERE match_id="+str(match_list[0])).fetchall()
-
-    participant_names = [i[0] for i in participants_data]
-    all_names = set(getData("player").values())
-
-    names = list(all_names.difference(set(participant_names)))
-    names.sort()
+    match_list, participants_data, names = Functions.match_webdeatils(matchday)
 
     return render_template('payment.html', match_list = match_list, participants_data = participants_data, n_players = len(participants_data), message = message, names = names, n_names = len(names))
 
@@ -361,21 +272,22 @@ def payment():
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     matchday = request.args['matchday']
-    matchday_id = [key for key, value in getData("match").items() if value[0] == matchday][0]
+    matchday_id = list(Functions.get_id("match",matchday).keys())[0]
     message = request.args['message']
     
     conn = sqlite3.connect('Volleyball.db')
     c = conn.cursor()
 
     if request.method == 'POST':
-
+        
         newby = 0
         signupname = request.form["uname"]
-        signup_firstname, signup_lastname = request.form["uname"].split()[0], ' '.join(request.form["uname"].split()[1:])
+        signup_firstname, signup_lastname = signupname.split()[0], ' '.join(signupname.split()[1:])
 
-        # Existence Check
-        search_list = [(key, value) for key, value in getData("player").items() if value == signupname]
-        if len(search_list) == 0:
+        status = Functions.signupcheck(matchday, signupname)
+
+        # NEW PERSON
+        if status == "unknown_p":
             if request.form.get("newby") == "on":
                 c.execute("INSERT INTO PLAYER (firstname, lastname) VALUES (?, ?)", (signup_firstname, signup_lastname))
                 conn.commit()
@@ -383,28 +295,23 @@ def signup():
             else:
                 return redirect(url_for('signup', matchday = matchday, message = "Speler niet gevonden, geef aan dat diegene nieuw is."))
         
-        # Capacity Check
-        max_limit = [i for i in list(getData("match").values()) if i[0]==matchday][0][4]
-        n_participants = len([i for i in list(getData().values()) if i[1] == matchday])
-        if n_participants >= max_limit:
+        # MATCH LIMIT REACHED
+        elif status == "full":
             return redirect(url_for('signup', matchday = matchday, message = "Limiet bereikt."))
 
-        # Duplicate Check
-        participants = [i for i in list(getData().values()) if i[1] == matchday]
-        for i in participants:
-            if i[0] == signupname:
-                return redirect(url_for('signup', matchday = matchday, message = signupname + " staat al ingeschreven."))
+        # PERSON ALREADY SIGNED UP
+        elif status == "duplicate":
+            return redirect(url_for('signup', matchday = matchday, message = signupname + " staat al ingeschreven."))
 
-        # Payment Check
-        player_id = c.execute(f"SELECT id FROM PLAYER WHERE firstname='{signup_firstname}' AND lastname='{signup_lastname}'").fetchall()[0][0]
-        print("---------------\n",player_id,"\n---------------\n")
-        player_history = [i for i in list(getData().values()) if i[0] == signupname]
-        print("---------------\n",player_history,"\n---------------\n")
-        payment_history = [0 if i[4] == 'NULL' else 1 for i in player_history]
-        print("---------------\n",payment_history,"\n---------------\n")
-
-        if sum(payment_history) != len(payment_history):
+        # PERSON DID NOT PAY
+        elif status == "not_paid":
             return redirect(url_for('signup', matchday = matchday, message = "Vorige betalingen niet voldaan."))
+
+        #SUCCESS
+        elif status == "allowed":
+            pass
+        
+        player_id = list(Functions.get_id("player",signupname).keys())[0]
 
         data = [(player_id, matchday_id,newby,0, 'NULL', 0)]
         c.executemany("INSERT INTO DATA (player_id, match_id, new_player, tentative, payment_id, paid) VALUES (?, ?, ?, ?, ?, ?)", data)
@@ -412,18 +319,7 @@ def signup():
         
         return redirect(url_for('signup', matchday = matchday, message = 'Je bent toegevoegd!'))
 
-    match_list = list(c.execute("SELECT * FROM MATCHDAYS WHERE date="+str(matchday)).fetchall()[0])
-    match_list.append(c.execute(f"SELECT details FROM LOCATIONS WHERE name='{match_list[4]}'").fetchall()[0][0])
-    participants_data = [i for i in list(getData().values()) if i[1]==matchday]
-    print(participants_data)
-    participants_data.sort(key=lambda row: (row[0]))
-    # player_data = c.execute("SELECT * FROM DATA WHERE match_id="+str(match_list[0])).fetchall()
-
-    participant_names = [i[0] for i in participants_data]
-
-    all_names = set(getData("player").values())
-    names = list(all_names.difference(set(participant_names)))
-    names.sort()
+    match_list, participants_data, names = Functions.match_webdeatils(matchday)
 
     return render_template('signup.html', match_list = match_list, participants_data = participants_data, n_players = len(participants_data), message = message, names = names, n_names = len(names))
 
